a0eb9272728624bde529e9366be243baa91b2431
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-08-25 15:50:38 -0300

    refatorado ex-5

--- exercicio-5.js
+++ exercicio-5.js
@@ -1,14 +1,24 @@
-let pirates = 19;
-let gold = 25;
+/* 5. Imagine que você é uma pessoa capitã de um barco e você precisa calcular se a sua viagem será bem sucedida. Você tem dois parâmetros numéricos inteiros, ouro e 
+piratas. Você terá sucesso na viagem se a quantidade de ouro for maior que a 
+quantidade de piratas. Mas se a soma de ouro e piratas for maior que 100, o navio 
+afunda. Retorne true se a viagem tiver sucesso e false se falhar */
 
-let booleanValue = pirates < gold;
+function pulaLinha(){
+    document.write("<br>")
+}
 
-function returnValue() {
-    if (booleanValue) {
-        return true;
-    } else {
+function returnValue(ouro, piratas) {
+
+    soma = ouro + piratas;
+
+    if(ouro > piratas){
+        return true
+    }else if(soma > 100){
+        return false
+    }else{
         return false;
     }
 }
-console.log('Aquantidade de ouro é: ' + returnValue());
+
+document.write(returnValue(100,20))
 
