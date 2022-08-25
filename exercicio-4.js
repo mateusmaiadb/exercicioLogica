6b06e2f1f902cfc969af032c8f673a028e788122
Mateus Pereira Rodrigues <86198294+mateusmaiadb@users.noreply.github.com>
2022-08-25 15:50:07 -0300

    refatorado ex-4

--- exercicio-4.js
+++ exercicio-4.js
@@ -1,21 +1,26 @@
-
+/* 4. Dada uma lista de bibliotecas, retorne:
+ "A", se o total de livros for maior ou igual a 1 e menor que 10
+ "B", se o total for maior ou igual a 10 e menor que 20
+ "C" se o número for maior ou igual 20 e menor que 30
+ "Z", se o número for igual a 0 */
 
 let lista = ['A', 'B', 'C','Z'];
 
-
-switch(lista){
-    case 'A':
-        mensagem = console.log(" total de livros for maior ou igual a 1 e menor que 10");
-        break;
-    case 'B':
-        mensagem = console.log("total for maior ou igual a 10 e menor que 20");
-        break;
-    case 'C':
-        mensagem = console.log("o número for maior ou igual 20 e menor que 30");
-        break;
-    case 'Z':
-        mensagem = console.log("o número for igual a 0");
-    
-    default:
-        console.log();
+function verificaCondicao(){
+    switch(lista){
+        case 'A':
+            mensagem = console.log(" total de livros for maior ou igual a 1 e menor que 10");
+            break;
+        case 'B':
+            mensagem = console.log("total for maior ou igual a 10 e menor que 20");
+            break;
+        case 'C':
+            mensagem = console.log("o número for maior ou igual 20 e menor que 30");
+            break;
+        case 'Z':
+            mensagem = console.log("o número for igual a 0");
+        
+        default:
+            console.log();
+    }
 }
